915e45930c2cd52c89a340098821de3e1a8a6323
MphoVersace <mtlomelompho@gmail.com>
2025-07-10 14:00:46 +0000

    FrontendJava Tasks 1

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,73 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+  
+  };
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+// Example usage:
+console.log(printTeacher("John", "Doe")); // Output: J. Doe
+
+
+console.log(director1);
+
+
+
+// Interface describing constructor parameters
+interface StudentClassConstructor {
+  firstName: string;
+  lastName: string;
+}
+
+// Interface describing the class
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+// Example usage:
+const student = new StudentClass('Moses', 'Ndlovu');
+console.log(student.displayName()); // Moses
+console.log(student.workOnHomework()); // Currently working
+
